486cb127154e009214d86e3102cc98212cba42c9
StevenCloud <stevencloud9@gmail.com>
2018-12-08 14:49:30 -0600

    Revert "Add files via upload"

--- s26.java
+++ s26.java
@@ -1,16 +1,17 @@
-package lecture3;
+package lecture2;
 import java.util.Scanner;
 
 public class s26 {
-	public static void main(String[] args)	{
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
-		System.out.printf("Please enter a number:");
-		double n = in.nextDouble();
-		
-		double ns = n * n;
-		
-		System.out.printf("The square of %.2f is %.2f.", n, ns);
-	}
+		System.out.printf("Please enter the radius: ");
 
+		Double radius = in.nextDouble();
+		double area = Math.PI * Math.pow(radius, 2);
+		
+		System.out.printf("The area is %f\n", area);
+			
+		
+	}
+	
 }
